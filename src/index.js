f4092117df1bde40c5eb6cea8cebac6d4b7c0a08
Thejus R <thejusr1999@gmail.com>
2022-08-23 18:37:15 +0530

    Updated index.js file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const dotenv = require("dotenv").config();
-// const connectDB = require("./config/db");
 const port = process.env.PORT;
+
+const app = express();
+
+connectDB();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const bookRouter = require("./routes/bookRoutes");
+app.use("/api/book", bookRouter);
+
+app.listen(port, () => {
+  console.log(`Server running in port ${port}`);
+});
