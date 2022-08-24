704e3e38fb91ce04ff552326b894d026927b680c
Thejus R <thejusr1999@gmail.com>
2022-08-24 16:13:07 +0530

    fixing cors issue #7

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv").config();
+const cors = require("cors");
 const port = process.env.PORT;
 
 const app = express();
 
 connectDB();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
